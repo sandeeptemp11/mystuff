397a9817ec335722ebd3424c844a816db0fa4455
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2023-07-17 08:51:14 +0530

    Create 6) Full pyramid.cpp

---
+++ 1) patterns/6) Full pyramid.cpp
@@ -0,0 +1,16 @@
+// Full pyramid
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n; // n >= 1
+    for(int i = n; i >= 1; i--){
+        for(int j = 1; j < i; j++) cout << " ";
+        for(int j = 1; j <= 2*(n - i + 1) - 1; j++){
+            if(j % 2) cout << "*";
+            else cout << " ";
+        }
+        cout << "\n";
+    }
+}
