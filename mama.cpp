f0fe7f2fc57b42e46e20e722312a9438c2239fc7
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2023-07-28 13:39:05 +0530

    Update mama.cpp

--- mama.cpp
+++ mama.cpp
@@ -1,25 +1,23 @@
 /*
-Here the sub-array is allowed to be empty
+Here the sub-array is aloowed to be empty
 */
-pair <long long, pair<int, int>> kadane(const int arr[], const int& n){
+pair <long long, pair<int, int>> kadane(int arr[], int& n){
     long long max_sum = 0;
     int s_max = 0;
     int l_max = -1;
 
     long long sum = 0;
     int s = 0;
-    int l = -1;
     for(int i = 0; i < n; i++){
         sum += arr[i];
-        l++;
-        while(sum < 0){
-            sum -= arr[s];
-            s++;
-        }
         if(max_sum < sum){
             max_sum = sum;
             s_max = s;
-            l_max = l;
+            l_max = i;
+        }
+        if(sum < 0){
+            sum = 0;
+            s = i + 1;
         }
     }
     return make_pair(max_sum, make_pair(s_max, l_max));
