99b1851859c3cb322425c8951188962ffd9de7b6
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2023-08-11 19:32:08 +0530

    Create maqbool.cpp

---
+++ alla/maqbool.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    bool f1= true, f2 = false;
+    f1 = ~f1;
+    f2 = ~f2;
+    cout << f1 << ", " << f2 << "\n"; // output is 1, 1 why??
+}
