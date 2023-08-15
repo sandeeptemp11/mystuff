fab1a0acc141e6888fd6da3f732bed0e0115eb6d
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2023-08-15 07:37:20 +0530

    Create program.cpp

---
+++ sandy/program.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct BinaryTreenode{
+    int n;
+    node* left;
+    node* right;
+} node;
+
+int main(){
+    // we can declare by both the names
+    BinaryTreenode ele;
+    node ele2;
+}
