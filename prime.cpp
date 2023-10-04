0486f3902543b2563cd494cc36ceee723a64200a
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2023-10-04 18:28:07 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,8 +1,31 @@
-#include <cmath>
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int floorSqrt(int n){
+	int low = 1;
+	int high = n;
+	while(low <= high){
+		int mid = (low + high)/2;
+		if(mid > n/mid) high = mid - 1;
+		else low = mid + 1;
+	}
+	return high;
+}
+
 bool isPrime(int n){
-    if(n <= 1) return false;
-    if(n == 2) return true; // edge case
-    for(int i = 2; i <= (int)sqrt(n); i ++){
-        if(n % i == 0) return false;
-    }return true;
+	if(n <= 1) return false;
+	int mySqrt = floorSqrt(n);
+	for(int i = 2; i <= mySqrt; i++){
+		if(n%i == 0){
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	isPrime(n) ? cout << "true" : cout << "false";
 }
