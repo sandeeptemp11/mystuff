cdfa0567cff8525e644f948bd6c8174d1bc56339
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2024-04-21 14:01:39 +0530

    Create prime_algo1.cpp

---
+++ prime_algo1.cpp
@@ -0,0 +1,28 @@
+// Seive of Eratosthenes algo 1
+// time complexity  :  n*log(log(n))
+// space complexity : O(n)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector <bool> is_prime(1+n, true);
+    is_prime[0] = is_prime[1] = false;
+    for(int p = 2; p <= n; p++){
+        if(is_prime[p]){
+            // p is prime
+            cout << p << " ";
+            for(int m = p*p; m <= n; m += p){
+                // m = p * j 
+                // where j >= p
+                // note that 1 <= j < p are already marked !!
+                is_prime[m] = false;
+            }
+        }
+    }
+    cout << "\n";
+}
