80ae8a7b780093dc70f27cd19eb895598c12ea3d
sandeeptemp11 <134224176+sandeeptemp11@users.noreply.github.com>
2024-06-10 10:21:29 +0530

    Update cyclic_sort.cpp

--- cyclic_sort.cpp
+++ cyclic_sort.cpp
@@ -1,13 +1,15 @@
 void cyclic_sort(int n, vector <int>& arr){
-    for(int cycle_start = 0; cycle_start <= n-2; cycle_start++){
-        while(1){
-            int pos = cycle_start;
-            for(int i = cycle_start + 1; i <= n-1; i++){
-                if(arr[i] <  arr[cycle_start]) pos++;
-            }
-            if(pos == cycle_start) break;
-            while(arr[pos] ==  arr[cycle_start]) pos++; 
-            swap(arr[cycle_start], arr[pos]);
+    int cycle_start = 0;
+    while(cycle_start <= n-2){
+        int pos = cycle_start;
+        for(int i = cycle_start + 1; i <= n-1; i++){
+            if(arr[i] <  arr[cycle_start]) pos++;
         }
+        if(pos == cycle_start){
+            cycle_start++;
+            continue;
+        }
+        while(arr[pos] ==  arr[cycle_start]) pos++; 
+        swap(arr[cycle_start], arr[pos]);
     }
 }
